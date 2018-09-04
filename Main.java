4a26ea56ad0d2d9e3459af8e1d3dee685f88dd7a
Miakarise <miax8005@stud.kea.dk>
2018-09-04 11:01:35 +0200

    GitTest

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Mr. From");
+        System.out.println("Hello Mr. Dubai");
     }
 }
